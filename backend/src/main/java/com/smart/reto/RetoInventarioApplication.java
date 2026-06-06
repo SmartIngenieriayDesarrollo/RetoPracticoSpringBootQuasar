9c5a6a6253de1ce6429a11032692911a06162ff5
SmartIngenieriayDesarrollo <alejandro@smartpos.com.co>
2026-06-06 13:51:40 -0500

    config inicial spring boot

---
+++ backend/src/main/java/com/smart/reto/RetoInventarioApplication.java
@@ -0,0 +1,12 @@
+package com.smart.reto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoInventarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetoInventarioApplication.class, args);
+    }
+}
